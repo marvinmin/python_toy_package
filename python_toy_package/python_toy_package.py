9ea79b6326ad7e16c499eff26eeecc0c4631a8bc
Marvin-chengmin <marvin.cmin@gmail.com>
2020-02-26 11:43:10 -0800

    added unit tests for sqsum

--- python_toy_package/python_toy_package.py
+++ python_toy_package/python_toy_package.py
@@ -1,4 +1,23 @@
 import numpy as np
 def sqsum(a):
+    """
+    Calculate the squared sum of a list
+
+    Parameters
+    ----------
+    a: list
+        the list of numbers to be calculated
+    
+    Return
+    ------
+    float
+        the squared sum
+    
+    Examples
+    --------
+    >>> a = [1, 2, 3]
+    >>> sqsum(a)
+    14
+    """
     a = np.array(a)
     return np.sum(a**2)
--- tests/test_python_toy_package.py
+++ tests/test_python_toy_package.py
@@ -1,1 +1,6 @@
 from python_toy_package import python_toy_package
+import numpy as np
+
+def test_sqsum():
+    a = np.array([1,2,3])
+    assert python_toy_package.sqsum(a) == 14
